8a468f0272d5e139480b3d01710f13134e580c6b
Tor Andre Haugdahl <thaugdahl@hotmail.com>
2023-09-12 18:07:22 +0200

    finding libraries

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <pthread.h>
+
+
+void *task(void *ptr) {
+
+    int a = (size_t) ptr;
+
+    printf("%d\n", a);
+
+}
 
 int main(
         __attribute((unused)) int argc,
@@ -6,6 +16,9 @@ int main(
         )
 {
     printf("Hello from jupiter!\n");
+
+    pthread_t tid;
+    pthread_create(&tid, 0, task, (void *) 30);
 
     return 0;
 }
