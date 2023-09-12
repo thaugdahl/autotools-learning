f5b3d295642a96867f884fe684705c667f0934dc
Tor Andre Haugdahl <thaugdahl@hotmail.com>
2023-09-12 13:56:32 +0200

    autoconf with substitution

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(
+        __attribute((unused)) int argc,
+        __attribute((unused)) char ** argv
+        )
+{
+    printf("Hello from jupiter!\n");
+
+    return 0;
+}
+
